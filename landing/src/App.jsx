df1e5e2de0a806dd1bc40a26d9ea9588e63d1562
Carlos A Palacios R <carlosargelio0104@gmail.com>
2023-12-13 16:30:51 -0400

    test: prettier #01

--- landing/src/App.jsx
+++ landing/src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
-    <>
+    <React.Fragment>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
-    </>
-  )
+    </React.Fragment>
+  );
 }
 
-export default App
+export default App;
